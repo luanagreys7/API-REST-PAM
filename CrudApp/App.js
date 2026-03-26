8b90779dfd1439f4a3089829ef95c023995ae715
luanagreys7 <luanagreys7@gmail.com>
2026-03-26 10:57:06 -0300

    fix: stack navigator

--- CrudApp/App.js
+++ CrudApp/App.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName={"Home"}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Create" component={CreateScreen} />
       </Stack.Navigator>
